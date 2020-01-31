394a4e387f96ff026ec9f2a2cd43781d10efcddb
a.schurenkov <a.schurenkov@dostaevsky.ru>
2020-01-31 16:57:51 +0300

    Edit config heroku

--- server/index.js
+++ server/index.js
@@ -15,11 +15,14 @@ app.use(bodyParser.json());
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
 
 app.use('/api', router);
 

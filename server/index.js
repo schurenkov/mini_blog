0fa06ecaa0ccd3cac9ad79c8d0ba673bca542395
a.schurenkov <a.schurenkov@dostaevsky.ru>
2020-01-31 16:55:58 +0300

    Edit config heroku

--- server/index.js
+++ server/index.js
@@ -15,8 +15,10 @@ app.use(bodyParser.json());
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + './client/build/index.html'));
+    res.sendFile(path.join(__dirname + 'index.html'));
 });
 
 app.use('/api', router);
